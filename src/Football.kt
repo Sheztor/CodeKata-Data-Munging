234ba1490743b114eb8d00381acc600c55d25c9d
Sheztor <hfsa2327@gmail.com>
2024-02-14 02:05:15 -0400

    Parte 2: Copiado y pegado del primer programa, con la logica un poco diferente al limpiar los datos a usar

--- src/Football.kt
+++ src/Football.kt
@@ -1,6 +1,29 @@
 import java.io.File
+import kotlin.math.abs
 
-fun football(){
+fun main(){
     val file = File("./src/football.dat")
 
+    val listOfLines = file.readLines()
+    val listGoalsDifferences = mutableListOf<Pair <String, Int>>()
+
+    for(line in listOfLines) {
+        var lineTrim = line.trim()
+
+        lineTrim = lineTrim.replace("-", "")
+        if(lineTrim.startsWith("Team") || lineTrim == "") continue
+
+        val valuesLine = lineTrim.split("\\s+".toRegex())
+
+        val teamName = valuesLine[1]
+        val goalsAgainst = valuesLine[6].filter { it.isDigit() }.toInt()
+        val goalsScored = valuesLine[7].filter { it.isDigit() }.toInt()
+
+        val valueDifference = abs(goalsAgainst - goalsScored)
+        listGoalsDifferences.add(Pair(teamName, valueDifference))
+    }
+
+    val result = listGoalsDifferences.minBy { it.second }
+
+    println("Team Name: ${result.first} \nMinor Difference: ${result.second}")
 }
