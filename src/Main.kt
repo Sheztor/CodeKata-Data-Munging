dde38f13de1f23d2ce9f3dc9b20f92d6b406a51c
Sheztor <hfsa2327@gmail.com>
2024-02-14 03:03:40 -0400

    Parte 3: codigo eliminado para simplificar a una funcion. Fin de este punto

--- src/Main.kt
+++ src/Main.kt
@@ -2,16 +2,17 @@ import java.io.File
 import kotlin.math.abs
 
 fun main() {
-
+    weather()
+    football()
 }
 
 fun process(file: File, nameProgram: String): Pair<String, Int> {
     val listOfLines = file.readLines()
     val listTempDifferences = mutableListOf<Pair <String, Int>>()
 
-    val value1 = if (nameProgram == "weather") 0 else 1
-    val value2 = if (nameProgram == "weather") 1 else 6
-    val value3 = if (nameProgram == "weather") 2 else 7
+    val indexList1 = if (nameProgram == "weather") 0 else 1
+    val indexList2 = if (nameProgram == "weather") 1 else 6
+    val indexList3 = if (nameProgram == "weather") 2 else 7
 
     for(line in listOfLines){
         var lineTrim = line.trim()
@@ -24,72 +25,27 @@ fun process(file: File, nameProgram: String): Pair<String, Int> {
             if(valuesLine[0].toIntOrNull() == null) continue
         }
 
-        val day = valuesLine[value1]
-        val maxTemp = valuesLine[value2].filter { it.isDigit() }.toInt()
-        val minTemp = valuesLine[value3].filter { it.isDigit() }.toInt()
+        val element = valuesLine[indexList1]
+        val value1 = valuesLine[indexList2].filter { it.isDigit() }.toInt()
+        val value2 = valuesLine[indexList3].filter { it.isDigit() }.toInt()
 
-        listTempDifferences.add(Pair(day, Differences(maxTemp, minTemp)))
+        listTempDifferences.add(Pair(element, abs(value1 - value2)))
     }
 
     val result = listTempDifferences.minBy { it.second }
     return result
 }
 
-fun Differences(value1: Int, value2: Int): Int{
-
-    return abs(value1 - value2)
-}
-
 fun weather(){
     val file = File("./src/weather.dat")
-
-    val listOfLines = file.readLines()
-    val listTempDifferences = mutableListOf<Pair <Int, Int>>()
-
-    for(line in listOfLines){
-        val lineTrim = line.trim()
-
-        if(lineTrim.startsWith("Dy") || lineTrim == "") continue
-
-        val valuesLine = lineTrim.split("\\s+".toRegex())
-
-        if(valuesLine[0].toIntOrNull() == null) continue
-
-        val day = valuesLine[0]
-        val maxTemp = valuesLine[1].filter { it.isDigit() }.toInt()
-        val minTemp = valuesLine[2].filter { it.isDigit() }.toInt()
-
-        listTempDifferences.add(Pair(day.toInt(), Differences(maxTemp, minTemp)))
-
-    }
-
-    val result = listTempDifferences.minBy { it.second }
+    val result = process(file, "weather")
 
     println("Number Day: ${result.first} \nMinor Difference : ${result.second}")
 }
 
 fun football(){
     val file = File("./src/football.dat")
-
-    val listOfLines = file.readLines()
-    val listGoalsDifferences = mutableListOf<Pair <String, Int>>()
-
-    for(line in listOfLines) {
-        var lineTrim = line.trim()
-
-        lineTrim = lineTrim.replace("-", "")
-        if(lineTrim.startsWith("Team") || lineTrim == "") continue
-
-        val valuesLine = lineTrim.split("\\s+".toRegex())
-
-        val teamName = valuesLine[1]
-        val goalsAgainst = valuesLine[6].filter { it.isDigit() }.toInt()
-        val goalsScored = valuesLine[7].filter { it.isDigit() }.toInt()
-
-        listGoalsDifferences.add(Pair(teamName, Differences(goalsAgainst, goalsScored)))
-    }
-
-    val result = listGoalsDifferences.minBy { it.second }
+    val result = process(file, "football")
 
     println("Team Name: ${result.first} \nMinor Difference: ${result.second}")
 }
