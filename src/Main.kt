1f17e5c2baf9971d7ae7ed5a9803ddee05dc236f
Sheztor <hfsa2327@gmail.com>
2024-02-14 02:18:05 -0400

    Parte 3: eliminé el archivo football.kt para tener ambas funciones en un mismo archivo, en este caso el Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,11 @@
 import java.io.File
+import kotlin.math.abs
 
 fun main() {
+
+}
+
+fun weather(){
     val file = File("./src/weather.dat")
 
     val listOfLines = file.readLines()
@@ -28,5 +33,31 @@ fun main() {
     val result = listTempDifferences.minBy { it.second }
 
     println("Number Day: ${result.first} \nMinor Difference : ${result.second}")
+}
 
+fun football(){
+    val file = File("./src/football.dat")
+
+    val listOfLines = file.readLines()
+    val listGoalsDifferences = mutableListOf<Pair <String, Int>>()
+
+    for(line in listOfLines) {
+        var lineTrim = line.trim()
+
+        lineTrim = lineTrim.replace("-", "")
+        if(lineTrim.startsWith("Team") || lineTrim == "") continue
+
+        val valuesLine = lineTrim.split("\\s+".toRegex())
+
+        val teamName = valuesLine[1]
+        val goalsAgainst = valuesLine[6].filter { it.isDigit() }.toInt()
+        val goalsScored = valuesLine[7].filter { it.isDigit() }.toInt()
+
+        val valueDifference = abs(goalsAgainst - goalsScored)
+        listGoalsDifferences.add(Pair(teamName, valueDifference))
+    }
+
+    val result = listGoalsDifferences.minBy { it.second }
+
+    println("Team Name: ${result.first} \nMinor Difference: ${result.second}")
 }
